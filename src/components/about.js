c56c4d56e66442d5b2bafce79accf64076accb47
Kathryn Hampton <khampton119@gmail.com>
2018-11-27 19:15:54 -0500

    minor css clean-up

--- src/components/about.js
+++ src/components/about.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <div className="About" id="about">
+      <div className="about">
         <h2>What I Do</h2>
-        <div className="Work">
+        <div className="work">
           <p>At work, I tell stories about people's TV and internet habits
             to convince businesses to advertise on cable. I'm currently working on expanding
             our proprietary news channel to 18 new markets across the country. </p>
@@ -14,15 +14,15 @@ class About extends Component {
             Through this experience I have discovered a passion for backend development,
             NodeJS and MongoDB.</p>
         </div>
-        <h3 font-size="150%">Tools I use</h3>
-        <div className="Skills">
+        <h3>Tools I use</h3>
+        <div className="skills">
           <ul>
             <li><i class="fab fa-js fa-5x" /></li>
             <li><i class="fab fa-react fa-5x" /></li>
             <li><i class="fab fa-node fa-5x" /></li>
           </ul>
         </div>
-        <div className="Res">
+        <div className="res">
           <p>You can access my resume 
           <a href="https://drive.google.com/open?id=18Wxkeod-XOgIxloaNerQ0UM_SVhBJnnc" target="_blank" rel="noopener noreferrer">
           <strong> here</strong></a></p>
